1c2cb83b57055dbc55e9a8ac695151340f174e6d
Arnald Puy <arnald.puy@gmail.com>
2019-03-04 12:05:26 +0100

    Increasing code coverage

--- tests/testthat/test-sobol_indices.R
+++ tests/testthat/test-sobol_indices.R
@@ -11,6 +11,8 @@ test_that("Error when second = FALSE and third = TRUE", {
                "The computation of third-order indices requires second = TRUE as it computes second-order indices first")
 })
 
+# FOR THE JANSEN ESTIMATOR
+
 test_that("number of rows equals k when second = FALSE & third = FALSE" , {
   N <- 100
   k <- 8
@@ -46,5 +48,19 @@ test_that("number of rows equals k + (!k / (!2 * !(k - 2))) +  (!k / (!3 * !(k -
                  (factorial(k) / (factorial(3) * factorial(k - 3))))
 })
 
+# FOR THE SALTELLI ESTIMATOR
+
+test_that("number of rows equals k + (!k / (!2 * !(k - 2))) +  (!k / (!3 * !(k - 3)))
+          when second = TRUE & third = TRUE" , {
+            N <- 100
+            k <- 8
+            R <- 10
+            A <- sobol_matrices(N, k, second = TRUE, third = TRUE)
+            params <- colnames(data.frame(A))
+            Y <- sobol_Fun(A)
+            expect_equal(nrow(sobol_indices(Y = Y, params = params, type = "saltelli", R = R, n = N, second = TRUE, third = TRUE)),
+                         k + (factorial(k) / (factorial(2) * factorial(k - 2))) +
+                           (factorial(k) / (factorial(3) * factorial(k - 3))))
+          })
 
 
